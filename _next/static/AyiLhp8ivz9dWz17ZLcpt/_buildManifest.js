b43eda717f8e9652e4dd83bece014cee92177440
THARUNJP <tharunroshini71@gmail.com>
2025-11-26 20:54:17 +0530

    Updates

--- _next/static/AyiLhp8ivz9dWz17ZLcpt/_buildManifest.js
+++ _next/static/AyiLhp8ivz9dWz17ZLcpt/_buildManifest.js
@@ -1,7 +1,12 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/portfolio-V1//_next/:path+",
+        "destination": "/portfolio-V1/_next/:path+"
+      }
+    ],
     "fallback": []
   },
   "sortedPages": [
