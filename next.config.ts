16c4ebff98cf3d04e2eaf9a9cc129d77b0870590
THARUNJP <tharunroshini71@gmail.com>
2025-11-26 20:36:52 +0530

    github pages setup completed

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
+const nextConfig:NextConfig = {
+  output: 'export',               // Required for GitHub Pages
+  images: {
+    unoptimized: true,            // Required for GH Pages
+  },
+  basePath: '/portfolio-V1',      // <-- replace this with your repo name
+  assetPrefix: '/portfolio-V1',   // <-- replace this with your repo name
 };
 
 export default nextConfig;
